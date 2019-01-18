18e7864373cf97b6f9279c8812b3e0eb14703f84
Henry Neal <henry.neal0@gmail.com>
2019-01-18 13:17:01 -0500

    moving on to cards from tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -26,7 +26,7 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    this.setState({ [tab.target.name]: tab.target.value })
+    this.setState({ selected: tab })
   };
 
   filterCards = (e) => {
@@ -59,7 +59,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} selectedTab={this.state.selectedTab} selectTabHandler={this.selectTabHandler}/>
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selectedTab} selectTabHandler={this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
